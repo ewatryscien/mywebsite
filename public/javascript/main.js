d73b47406a6dc4ef8494e6ade87fa7ab00164b7a
Ewa Tryscien <ewaat007@gmail.com>
2020-06-02 20:07:52 +0100

    event on click button - change theme

---
+++ public/javascript/main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("change").addEventListener("click", function() {
+        document.body.classList.toggle("dark-theme");
+    });
+});
