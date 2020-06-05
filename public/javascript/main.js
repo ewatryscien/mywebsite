9a25768913eb5fb1a9e21bf558e6675397ea8bab
Ewa Tryscien <ewaat007@gmail.com>
2020-06-05 01:30:06 +0100

    dark theme function

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("change").addEventListener("click", function() {
-        document.body.classList.toggle("dark-theme");
+    let about = document.getElementById("about");
+    let work = document.getElementById("work");
+    let contact = document.getElementById("contact");
+
+    document.getElementById("theme").addEventListener("click", function() {
+        //document.body.classList.toggle("dark-theme");
+        about.classList.toggle("dark");
+        work.classList.toggle("dark");
+        contact.classList.toggle("dark");
+
     });
 });
+/*
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("about-circle").addEventListener("click", function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            let hash = this.hash;
+  
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+    */
