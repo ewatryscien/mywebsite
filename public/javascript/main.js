b2689119fdb611758313fa4a59b5b48571802d80
Ewa Tryscien <ewaat007@gmail.com>
2020-06-15 00:20:28 +0100

    Updated content and fixed scrolling

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let about = document.getElementById("about");
     let work = document.getElementById("work");
     let contact = document.getElementById("contact");
-
+/*
     document.getElementById("theme").addEventListener("click", function() {
         //document.body.classList.toggle("dark-theme");
         about.classList.toggle("dark");
@@ -10,21 +10,10 @@ document.addEventListener('DOMContentLoaded', function () {
         contact.classList.toggle("dark");
 
     });
+*/
+    setTimeout(function() {
+        let html = document.getElementsByTagName("html");
+        html[0].style.scrollBehavior = "smooth";
+    }, 500);
 });
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("about-circle").addEventListener("click", function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
 
-            let hash = this.hash;
-  
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
-});
-    */
