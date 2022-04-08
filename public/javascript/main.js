20c2dcab614a102080b1eb6d61569c9cfef905c7
ewatryscien <ewa.tryscien@gmail.com>
2022-04-08 01:53:50 +0100

    small changes in styling and text

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,19 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let about = document.getElementById("about");
-    let work = document.getElementById("work");
-    let contact = document.getElementById("contact");
-/*
-    document.getElementById("theme").addEventListener("click", function() {
-        //document.body.classList.toggle("dark-theme");
-        about.classList.toggle("dark");
-        work.classList.toggle("dark");
-        contact.classList.toggle("dark");
+document.addEventListener("DOMContentLoaded", function () {
+  let about = document.getElementById("about");
+  let work = document.getElementById("work");
+  let contact = document.getElementById("contact");
 
-    });
-*/
-    setTimeout(function() {
-        let html = document.getElementsByTagName("html");
-        html[0].style.scrollBehavior = "smooth";
-    }, 500);
+  setTimeout(function () {
+    let html = document.getElementsByTagName("html");
+    html[0].style.scrollBehavior = "smooth";
+  }, 500);
 });
-
